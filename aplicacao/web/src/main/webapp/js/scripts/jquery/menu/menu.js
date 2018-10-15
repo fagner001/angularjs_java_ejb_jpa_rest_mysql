ad23d68ef647d4693811add9662cd4649f35b32c
Administrador <Administrador@DESKTOP-9RGEQF4>
2018-10-15 00:14:10 -0300

    Fagner: Excluindo projeto com erro nas configurações do maven

--- aplicacao/web/src/main/webapp/js/scripts/jquery/menu/menu.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function(){
-    $(".dropdown").on(
-        function() { $('.dropdown-menu', this).fadeIn("fast");
-        },
-        function() { $('.dropdown-menu', this).fadeOut("fast");
-    });
-});
--- aplicacao/web/src/main/webapp/js/service/config.js
+++
@@ -1,3 +0,0 @@
-angular.module('myApp').value('config',{
-  baseUrl : 'http://localhost:8888/aplicacao-web/services/produto'
-});
